46a90aae10bec794780ddf4565da66d8f05fe783
manoj <m64938096@gmail.com>
2026-07-10 23:24:40 +0530

    update all

--- REACT/DAY6/rendering/src/App.jsx
+++ REACT/DAY6/rendering/src/App.jsx
@@ -1,10 +1,40 @@
 
-import React from 'react'
+function App() {
+  const students = [
+    {
+      id: 1,
+      name: "Manoj",
+      age: 22,
+      course: "React",
+    },
+    {
+      id: 2,
+      name: "Rahul",
+      age: 21,
+      course: "JavaScript",
+    },
+    {
+      id: 3,
+      name: "dhoni",
+      age: 23,
+      course: "HTML & CSS",
+    },
+  ];
 
-const App = () => {
   return (
-    <div className='bg-gray-200 p-10 '>App</div>
-  )
+    <div className="bg-yellow-300">
+      <h1>Students List</h1>
+
+      {students.map((student) => (
+        <div key={student.id}>
+          <h2>{student.name}</h2>
+          <p>Age: {student.age}</p>
+          <p>Course: {student.course}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
